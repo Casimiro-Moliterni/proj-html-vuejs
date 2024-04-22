7f6082b6d5a8e5a345585cd29ab256182938277e
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-04-22 18:55:12 +0200

    add header bottom + img

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,78 @@
 <script>
 export default {
-  name: 'PageHeader'
+  name: 'PageHeader',
+  data() {
+    return {
+      socialIcon: [
+        {
+          id: '01',
+          name: 'fa-facebook-f',
+          color: 'color-fb'
+        },
+        {
+          id: '02',
+          name: 'fa-twitter',
+          color: 'color-tw'
+        },
+        {
+          id: '03',
+          name: 'fa-linkedin-in',
+          color: 'color-in'
+        },
+        {
+          id: '04',
+          name: 'fa-instagram',
+          color: 'color-fb'
+        },
+        {
+          id: '05',
+          name: 'fa-google-plus-g',
+          color: 'color-gp'
+        },
+      ],
+      navList:[
+        {
+          name:'Home',
+          id:'01',
+          icon:'fa-solid fa-caret-down'
+        },
+        {
+          name:'Celebrity',
+          id:'02',
+          icon:'fa-solid fa-caret-down'
+        },
+        {
+          name:'Movie',
+          id:'03',
+          icon:'fa-solid fa-caret-down'
+        },
+        {
+          name:'Page',
+          id:'04',
+          icon:'fa-solid fa-caret-down'
+        },
+        {
+          name:'shop',
+          id:'05',
+          icon:'fa-solid fa-caret-down'
+        },
+        {
+          name:'blog',
+          id:'06',
+        },
+        {
+          name:'Contact Us',
+          id:'07'
+        },
+      ]
+
+    }
+  },
+  methods: {
+    getImageUrl(name) {
+      return new URL(`../assets/img/${name}`, import.meta.url).href;
+    },
+  }
 }
 </script>
 
@@ -13,13 +85,12 @@ export default {
           <i class="fa-regular fa-envelope"></i>
           <span>Superhit Top Movie</span>
           <span id="name">**King Star**</span>
-
           <ul>
-            <li><a href="#" class="color-fb"><i class="fa-brands fa-facebook-f"></i></a></li>
-            <li><a href="#" class="color-tw"><i class="fa-brands fa-twitter"></i></a></li>
-            <li><a href="#" class="color-in"><i class="fa-brands fa-linkedin-in"></i></a></li>
-            <li><a href="#" class="color-ig"><i class="fa-brands fa-instagram"></i></a></li>
-            <li><a href="#" class="color-gp"><i class="fa-brands fa-google-plus-g"></i></a></li>
+            <li v-for="singleIcon in socialIcon">
+              <a href="#">
+                <i :class="[`fa-brands ${singleIcon.name} ${singleIcon.color}`]"></i>
+              </a>
+            </li>
           </ul>
         </div>
         <div class="login">
@@ -31,13 +102,32 @@ export default {
     <!-- header-center-input  -->
     <div class="header-center">
       <div class="container">
-
+        <div class="image">
+          <img :src="getImageUrl('cropped-logo.png')" alt="">
+        </div>
+        <div class="search-bar">
+          <input type="text" placeholder="search...">
+          <div>Search</div>
+        </div>
       </div>
     </div>
     <!-- header-bottom  -->
     <div class="header-bottom">
       <div class="container">
-
+        <nav>
+          <ul>
+            <li v-for="singlElement in navList">
+              <a href="#">
+                {{ singlElement.name }}
+              </a>
+              <i :class="[` ${singlElement.icon}`]"></i>
+            </li>
+          </ul>
+          <div class="basket-buy">
+            <div>0</div>
+            <i class="fa-solid fa-basket-shopping"></i>
+          </div>
+        </nav>
       </div>
     </div>
   </header>
@@ -46,53 +136,154 @@ export default {
 <style scoped lang="scss">
 @use '../style/partials/_variables' as *;
 
-.header-top {
+header {
   background-color: $brand_secondary;
   color: white;
+}
+
+.header-top {
+  color: white;
   padding: 18px 0;
-  border-bottom: 1px soldid #182028;
-     .container{
-     display: flex;
-     align-items: center;
+  border-bottom: 1px solid #182028;
+
+  .container {
+    display: flex;
+    align-items: center;
+    padding-right: 15px;
+
     //  email-text + icon social
-      .email-text,ul,.login{
+    .email-text,
+    ul,
+    .login {
       display: flex;
       align-items: center;
-      font-size:14px ;
-      .fa-envelope{
+      font-size: 14px;
+
+      .fa-envelope {
         margin-right: 4px;
       }
-      ul{
+
+      ul {
         gap: 10px;
       }
+
       #name {
         color: $brand_primary;
         margin-right: 20px;
       }
-     }
+    }
 
     //  login 
-    .login{
+    .login {
       margin-left: auto;
       gap: 7px;
     }
-   }
-    
 
-     .color-fb{
+    .color-fb {
       color: #3b5998;
-     }
-     .color-tw{
+    }
+
+    .color-tw {
       color: #00acee;
-     }
-     .color-in{
+    }
+
+    .color-in {
       color: #0e76a8;
-     }
-     .color-ig{
+    }
+
+    .color-ig {
       color: #a01e28;
-     }
-     .color-gp{
+    }
+
+    .color-gp {
       color: #db4a39;
+    }
+  }
+}
+
+.header-center {
+  padding: 12px 0 32px 0;
+border-bottom:1px solid #182028;
+  .container {
+    display: flex;
+    justify-content: space-between;
+
+    .image {
+      height: 65px;
+
+      img {
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    .search-bar {
+      width: 23%;
+      height: 50px;
+      display: flex;
+      justify-content: flex-end;
+
+      input {
+        width: 100%;
+        padding-left: 10px;
+        height: 100%;
+        border: none;
+        background-color: inherit;
+        border-top: 1px solid #182028;
+        border-bottom: 1px solid #182028;
+
+      }
+
+      div {
+        display: flex;
+        background-color: $brand_primary;
+        height: 100%;
+        align-items: center;
+        padding: 0 20px;
+      }
+    }
+  }
+}
+
+.header-bottom {
+padding: 14px 0px 26px 0;
+  .container {
+    display: flex;
+    align-items: center;
+    
+     nav{
+      display: flex;
+      align-items: center;
+      width: 100%;
+      ul{
+        display: flex;
+        flex-grow:1 ;
+        gap: 20px;
+        a{
+          color: white;
+          font-size: 24px;
+          margin-right: 4px;
+          font-weight: 700;
+        }
+      }
+       .basket-buy{
+         display: flex;
+         flex-direction: column;
+         justify-content: end;
+          div{
+            margin-left: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 14px;
+            background-color: $brand_primary;
+            padding: 6px;
+            height: 20px;
+            border-radius: 100%;
+          }
+       }
+      
      }
+  }
 }
 </style>
